9ef7863d8fc327be4475f31eea49647db5997cd9
root <foogundipe@student.oauife.edu.ng>
2022-09-13 16:46:43 -0700

    enforce betty rules

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,6 +11,7 @@ void times_table(void)
 
 	while (i < 10)
 	{
+		_putchar(48);
 		for (j = 1; j < 10; j++)
 		{
 			product = i * j;
@@ -21,7 +22,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(product % 10 + '0');
 			}
-			else 
+			else
 			{
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
