33086f7f6dcdb50035ba0197c0c9e66045f6a04f
root <foogundipe@student.oauife.edu.ng>
2022-09-13 15:06:38 -0700

    print absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * _abs - function to compute the absolute value of an integer
+ *
+ * @j: the argument to be passed into the function
+ * Return: the absolute number
+ */
+int _abs(int j)
+{
+	int a  = abs(j);
+	return (a);
+}
