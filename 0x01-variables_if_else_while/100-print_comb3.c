af599862f42579ec68c86a35d92ff6366e73510b
root <foogundipe@student.oauife.edu.ng>
2022-09-13 12:25:05 -0700

    task 0

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-		if (i != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}		
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');		
 	}	
 	putchar('\n');
 	return (0);
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Success 0 (Always)
+ */
+int main(void)
+{
+	char *ch = "_putchar";
+	_putchar(*ch);
+	_putchar('\n');
+	return (0);
+}
