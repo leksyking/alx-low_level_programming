34c341f0545eb6ffc19807ec22af2bf8133f167d
root <foogundipe@student.oauife.edu.ng>
2022-09-13 16:10:48 -0700

    function to print every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function to print time
+ */
+void jack_bauer(void)
+{
+	int hr2, hr1, min2, min1;
+	int t = 9;
+
+	hr2 = 0;
+	while (hr2 <= 2)
+		if (hr2 == 2)
+			t = 3;
+		hr1 = 0;
+		while (hr1 <= t)
+			min2 = 0;
+			while (min2 <= 5)
+				min1 = 0;
+				while (min1 <= 9)
+					_putchar('0' + hr2);
+					_putchar('0' + hr1);
+					_putchar(':');
+					_putchar('0' + min2);
+					_putchar('0' + min1);
+					_putchar('\n');
+					min1++;
+				min2++;
+			hr1++;
+		hr2++;
+}
+
