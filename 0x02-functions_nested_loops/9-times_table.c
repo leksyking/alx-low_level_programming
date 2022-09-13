b0950e2ebe281521f3337b040c4542feae214ba1
root <foogundipe@student.oauife.edu.ng>
2022-09-13 16:41:22 -0700

    remove the duplicate

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -43,39 +43,7 @@ void times_table(void)
 
 	while (i < 10)
 	{
-		for (j = 1; j < 10; J++)
-		{
-			product = i * j;
-			_putchar(',');
-			_putchar(' ');
-			if (product < 10)
-			{
-				_putchar(' ');
-				_putchar(product % 10 + '0');
-			}
-			else 
-			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
-			}
-		}
-		_putchar('\n');
-		i++;
-	}
-}#include "main.h"
-
-/**
- * times_table -  function name
- *
- * Return: Always 0
- */
-void times_table(void)
-{
-	int i, j, product;
-
-	while (i < 10)
-	{
-		for (j = 1; j < 10; J++)
+		for (j = 1; j < 10; j++)
 		{
 			product = i * j;
 			_putchar(',');
