a52868b49f3865144ca1d7490b9b9c3b57134a25
root <foogundipe@student.oauife.edu.ng>
2022-09-13 17:04:58 -0700

    print numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - function to print from n t0 98
+ *
+ * @n: argument
+ *
+ */
+void print_to_98(int n)
+{
+	for (int i = n; i <= 98; i++)
+	{
+		printf("%d, ", i);
+	}
+}
