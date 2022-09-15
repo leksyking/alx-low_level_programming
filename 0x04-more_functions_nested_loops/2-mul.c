868f6dc5da8ec09f9e26955278f647c67ad69c65
root <foogundipe@student.oauife.edu.ng>
2022-09-15 07:51:59 -0700

    add a function to multiply numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies the integers
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
