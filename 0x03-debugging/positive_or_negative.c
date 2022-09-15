e7b8c44c7cf0e7c11de84dd144a3cf9590a59f64
root <foogundipe@student.oauife.edu.ng>
2022-09-15 00:00:22 -0700

    run the program

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,17 +9,12 @@
 /* betty style doc for function main goes there */
 /**
  * positive_or_negative - function to check the signs of numbers
- * @int: argument
+ * @n: argument
  * Return: Always 0 (Success)
  */
 
-int positive_or_negative(int)
+int positive_or_negative(int n)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
