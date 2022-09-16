718c39e73d1b29af5c9612429bef3f5fb375e041
root <foogundipe@student.oauife.edu.ng>
2022-09-15 18:25:09 -0700

    last mandatory task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints triangle
+ *
+ * @size: parameter - size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i < size; i++)
+		{
+			j = 1;
+			k = 1;
+			while (j < size)
+			{
+				_putchar(' ');
+				j++;
+			}
+			while (k <= i)
+			{
+				_putchar('#');
+				k++;
+			}
+			_putchar('\n');
+		}
+	}
+}
