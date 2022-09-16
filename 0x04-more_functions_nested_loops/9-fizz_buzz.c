4ec9c1766c7fd5639513cfca3f8d7d7eed8769c2
root <foogundipe@student.oauife.edu.ng>
2022-09-15 17:04:37 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz buzz code
+ *
+ * Return: Always o.
+ */
+int main(void)
+{
+	int i;
+	char *n;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			n = "Fizz";
+			printf("%s ", n);
+		}
+		else if ((i % 5) == 0)
+		{
+			n = "Buzz";
+			printf("%s ", n);
+		}
+		else if (((i % 5) == 0) && ((i % 3) == 0))
+		{
+			n = "FizzBuzz";
+			printf("%s ", n);
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
