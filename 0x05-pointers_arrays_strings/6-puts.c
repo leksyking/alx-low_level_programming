174e82abf49b43b708b9da919a771f27aa68c833
root <foogundipe@student.oauife.edu.ng>
2022-09-19 15:04:43 -0700

    print every other character starting with the first line

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print every other character starting with the first one
+ *
+ * @str: parameter
+ */
+
+void puts2(char *str)
+{
+	int i, len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
