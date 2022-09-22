de3639db262bf2cbd2ec9be4d87f6ea838c857f2
root <foogundipe@student.oauife.edu.ng>
2022-09-22 11:54:22 -0700

    task 2 on copying strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies string
+ *
+ * @dest: string to be copied into
+ * @src: string to copy
+ * @n: number of bytes
+ *
+ * Return: the copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
