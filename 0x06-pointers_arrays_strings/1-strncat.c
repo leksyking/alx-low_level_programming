8e11f890004dfe1389f4810ecd12e7acaf780e4a
root <foogundipe@student.oauife.edu.ng>
2022-09-22 04:19:51 -0700

    refactor stask 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j, m;
 
-	for (m = 0; dest[m] != '\0'; m++);
+	m = 0;
+	while (dest[m] != '\0')
+	{
+		m++;
+	}
 	for (i = m, j = 0; j <= n - 1; i++, j++)
 	{
 		dest[i] = src[j];
