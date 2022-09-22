cf177eeeda8e58d90a19bb842eeab0db98e5e3d4
root <foogundipe@student.oauife.edu.ng>
2022-09-22 13:42:53 -0700

    add numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,15 +17,15 @@ char *leet(char *str)
 	for (i = 0; i < n; i++)
 	{
 		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
+			str[i] = 4;
 		else if (str[i] == 'e' || str[i] == 'E')
-			str[i]  = '3';
+			str[i]  = 3;
 		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
+			str[i] = 0;
 		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
+			str[i] = 7;
 		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+			str[i] = 1;
 	}
 	return (str);
 }
