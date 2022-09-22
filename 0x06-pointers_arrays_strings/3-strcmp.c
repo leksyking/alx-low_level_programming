c2dc9144ad5afff6a624c9b1a41b426f52b2b2df
root <foogundipe@student.oauife.edu.ng>
2022-09-22 04:51:25 -0700

    add task3 to compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,33 @@
 #include "main.h"
 
+/**
+ * _strcmp - function that compares two string
+ *
+ * @s1: first parameter
+ * @s2: second parameter
+ *
+ * Return: 15, -15 or 0
+ */
 int _strcmp(char *s1, char *s2)
 {
-    int i, value;
+	int i, value;
 
-    for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-    {
-        if (s1[i] == '\0')
-        {
-            value = -15;
-            break;
-        }
-        else if (s2[i] == '\0')
-        {
-            value = 15;
-            break;
-        }
-        else if (s2[i] == s1[i])
-        {
-            value = 0;
-        }
-    }
-    return (value);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] == '\0')
+		{
+			value = -15;
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			value = 15;
+			break;
+		}
+		else if (s2[i] == s1[i])
+		{
+			value = 0;
+		}
+	}
+	return (value);
 }
