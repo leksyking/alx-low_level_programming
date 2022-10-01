adcee379d94e26f3693daf32bb6823a0f97ac696
root <foogundipe@student.oauife.edu.ng>
2022-09-30 20:18:37 -0700

    program that multiplies argumenst

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that multiplies 2 numbers of the cdl
+ *
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: Error or the result of the multiplication followed
+ * by 1 or 0 respectively
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
