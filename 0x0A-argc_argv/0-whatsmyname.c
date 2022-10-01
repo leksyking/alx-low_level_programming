326b946f3b59a63fe45144baff8852a1bf01c03d
root <foogundipe@student.oauife.edu.ng>
2022-09-30 19:49:40 -0700

    add task 0 to print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program thta prints its name
+ * followed by a new line
+ *
+ * @argc: number of cdl arguments
+ * @argv: array of cdl arguments
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0 * argc]);
+	return (0);
+}
