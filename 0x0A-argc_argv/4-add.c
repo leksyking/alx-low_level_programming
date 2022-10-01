61ccd926de13b17490a196b4d59f1aa0168edf7f
root <foogundipe@student.oauife.edu.ng>
2022-09-30 21:16:01 -0700

    fix it

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 		for (j = 0; j < length; j++)
 		{
-			if (isdigit(*argv[j]) == 0)
+			if (isdigit(*(ptr + j)) == 0)
 			{
 				printf("Error\n");
 				return (1);
