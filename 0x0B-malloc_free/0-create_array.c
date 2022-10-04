364b6e73e6204cd0b5b25e10331cc67882cb10a5
root <foogundipe@student.oauife.edu.ng>
2022-10-04 12:17:36 -0700

    fix a bug

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ch;
+	unsigned int i;
 
 	ch = malloc(sizeof(char) * size);
 	if (size == 0)
@@ -23,6 +24,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	ch[0] = c;
+	for (i = 0; i < size; i++)
+		ch[i] = c;
 	return (ch);
 }
