5c53f40cd4a9368b73ce0a9a2f5c8d6538eeb146
root <foogundipe@student.oauife.edu.ng>
2022-10-04 13:17:13 -0700

    add parentheses

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	ch = malloc((sizeof(char) * (strlen(s1) + strlen(s2))) + 1);
 	if (ch == NULL)
 		return (NULL);
-	for (i = 0; i < (strlen(s1) + strlen(s2); i++))
+	for (i = 0; i < (strlen(s1) + strlen(s2)); i++)
 	{
 		if (i < strlen(s1))
 		{
