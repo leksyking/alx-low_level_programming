d2a85151fb2ad327b6c473f4a4430bbe2e12080d
root <foogundipe@student.oauife.edu.ng>
2022-10-06 14:37:42 -0700

    refactor task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,26 +1,26 @@
-#include "main.h"                                                                                                           
-#include <stdlib.h>                                                                                                         
-#include <stdio.h>                                                                                                          
-                                                                                                                            
-/**                                                                                                                         
- * array_range - function that creates an array                                                                             
- *                                                                                                                          
- * @min: where array should start                                                                                           
- * @max: where array should stop                                                                                            
- *                                                                                                                          
- * Returm: array                                                                                                            
- */                                                                                                                         
-int *array_range(int min, int max)                                                                                          
-{                                                                                                                           
-        int i, val, *ptr;                                                                                                        
-                                                                                                                            
-        if (min > max)                                                                                                      
-                return (NULL);                                                                                              
-        val = (max - min) + 1;                                                                                              
-        ptr = malloc(sizeof(int) * val);                                                                                    
-        if (ptr == NULL)                                                                                                    
-                return (NULL);                                                                                              
-        for (i = 0; i < val; i++)                                                                                          
-                ptr[i] = min;                                                                                               
-                min++;                                                                                                      
-}               
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function that creates an array
+ *
+ * @min: where array should start
+ * @max: where array should end
+ *
+ * Return: nothing
+ */
+int *array_range(int min, int max)
+{
+	int val, i, *ptr;
+
+	if (min > max)
+		return (NULL);
+	val = (max - min) + 1;
+	ptr = malloc(sizeof(int) * val);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < val; i++)
+		ptr[i] = min;
+		min++;
+	return (ptr);
+}
