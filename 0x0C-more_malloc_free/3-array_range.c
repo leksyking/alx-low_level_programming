9da185269b7b3cf706b66b0d2d407556a055cec9
root <foogundipe@student.oauife.edu.ng>
2022-10-06 14:41:58 -0700

    fix some bugs

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,18 +1,18 @@
-<<<<<<< HEAD
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * array_range - function that creates an array
  *
  * @min: where array should start
- * @max: where array should end
+ * @max: where array should stop
  *
- * Return: nothing
+ * Return: array
  */
 int *array_range(int min, int max)
 {
-	int val, i, *ptr;
+	int i, *ptr, val;
 
 	if (min > max)
 		return (NULL);
@@ -20,36 +20,8 @@ int *array_range(int min, int max)
 	ptr = malloc(sizeof(int) * val);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < val; i++)
+	for (i = 0; i <= max; i++)
 		ptr[i] = min;
 		min++;
 	return (ptr);
 }
-=======
-#include "main.h"                                                                                                           
-#include <stdlib.h>                                                                                                         
-#include <stdio.h>                                                                                                          
-                                                                                                                            
-/**                                                                                                                         
- * array_range - function that creates an array                                                                             
- *                                                                                                                          
- * @min: where array should start                                                                                           
- * @max: where array should stop                                                                                            
- *                                                                                                                          
- * Returm: array                                                                                                            
- */                                                                                                                         
-int *array_range(int min, int max)                                                                                          
-{                                                                                                                           
-        int i, val, *ptr = NULL;                                                                                                        
-                                                                                                                            
-        if (min > max)                                                                                                      
-                return (NULL);                                                                                              
-        val = (max - min) + 1;                                                                                              
-        ptr = malloc(sizeof(int) * val);                                                                                    
-        if (ptr == NULL)                                                                                                    
-                return (NULL);                                                                                              
-        for (i = 0; i < val; i++)                                                                                          
-                ptr[i] = min;                                                                                               
-                min++;                                                                                                      
-}               
->>>>>>> 576916744c5b82fb34dd35dc9f9888f0f64f216a
