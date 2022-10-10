dc45ac3147fe3aa6f74f3d88616b3e915e0100b5
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2022-10-11 00:31:21 +0100

    fix some bugs in names

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,13 +17,13 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age == NULL)
-		printf("Name: (nil)\n");
+		printf("Age: (nil)\n");
 	else
-		printf("Name: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("Name: (nil)\n");
+		printf("Owner: (nil)\n");
 	else
-		printf("Name: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 
 }
 
