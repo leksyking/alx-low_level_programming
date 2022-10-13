937632424612deac488eebd3c55bcd8c972704ff
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2022-10-13 22:31:42 +0100

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,21 +19,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
+		x = va_arg(args, char *);
+		if (x == NULL)
 		{
-			x = va_arg(args, char *);
-			if (x == NULL)
-			{
-				x = "(nil)";
-			}
-			if (i != (n - 1))
-			{
-				printf("%s%s", x, separator);
-			}
-			else
-			{
-				printf("%s", x);
-			}
+			printf("(nil)");
+		}
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s%s", x, separator);
+		}
+		else
+		{
+			printf("%s", x);
 		}
 	}
 	va_end(args);
