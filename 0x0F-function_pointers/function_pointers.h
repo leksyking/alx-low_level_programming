02432f6ee0f9743572f011a655442a406bc891cf
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2022-10-13 17:15:03 +0100

    fix a bug here

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,4 +7,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
-#end /* FUNCTION_POINTERS_H */
+#endif /* FUNCTION_POINTERS_H */
