8d20d53b26b7af2cc1f95e8567458b1e01418b6b
root <foogundipe@student.oauife.edu.ng>
2022-10-31 14:01:31 -0700

    add task 2 to append text to files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - a function that appends text at the
+ * of a file
+ * @filename: file to append text to.
+ * @text_content: text to append to file
+ * Return: 1 on success or -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd = 0, wr = 0, i = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+		wr = write(fd, text_context, i);
+		if (wr < 0)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
