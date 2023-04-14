12e818f3583129511a3d559a4f1c158e2761de65
root <foogundipe@student.oauife.edu.ng>
2023-04-13 20:45:02 -0700

    update the c file

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,30 @@
-#includes
+#include "search_algos.h"
+
+/**
+ * linear_search - a function that searches for a value
+ *  in an array of integers using the Linear search algorithm
+ *
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to search in the array
+ *
+ * Return: the first index where the value is located else -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t n;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (n = 0; n < size; n++)
+	{
+		printf("Value checked array[%li] = [%i]\n", n, array[n]);
+		if (array[n] == value)
+			return (n);
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -18,7 +18,7 @@ typedef struct listint_s
 	int n;
 	size_t index;
 	struct listint_s *next;
-} listint_s;
+} listint_t;
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
@@ -35,8 +35,8 @@ typedef struct skiplist_s
 	int n;
 	size_t index;
 	struct skiplist_s *next;
-	struct skiplist_s express;
-}
+	struct skiplist_s *express;
+} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
